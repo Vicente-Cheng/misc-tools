087b0f9fe436c3de6277efe87906ef494b83e773
Vicente Cheng <freeze.bilsted@gmail.com>
2017-02-15 11:28:00 +0800

    dlopen_tester could check shared library with dlopen

---
+++ dlopen_tester.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <dlfcn.h>
+
+int main(int argc, char **argv) {
+    void *handle;
+    double (*cosine)(double);
+    char *error;
+
+    printf("try to open: %s\n", argv[1]);
+
+    handle = dlopen(argv[1], RTLD_LAZY);
+    if (!handle) {
+        fputs (dlerror(), stderr);
+        exit(1);
+    }
+
+    printf("successfully!\n");
+
+    return 1;
+}
